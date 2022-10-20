e21af575e4f7fc085497e8060fd7a0d80aa7073d
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-10-20 22:38:33 +0300

    create post page setup and styling

--- src/Pages/CreatePost.js
+++ src/Pages/CreatePost.js
@@ -2,6 +2,19 @@
 
 export default function CreatePost(){
     return(
-        <div>Create Post</div>
+        <div className="createPostPage">
+        <div className="cpContainer">
+        <h1>Create A Post</h1>
+        <div className="inputGp"> 
+        <label>Title:</label>
+        <input placeholder="Title..."/>
+        </div>
+        <div className="inputGp">
+        <label>Post:</label>
+        <textarea placeholder="Post..."/>
+        </div>
+        <button>Submit Post</button>
+        </div>
+        </div>
     )
 }
